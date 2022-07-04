74fbf208d3674d127b0d6224b814a37d3037e049
Wongi <hpurs95@gmail.com>
2022-07-04 16:41:46 +0900

    Initial Commit

---
+++ MultipleFunc/MultipleFuncApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleFuncApp.swift
+//  MultipleFunc
+//
+//  Created by MacBook Air on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct MultipleFuncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
